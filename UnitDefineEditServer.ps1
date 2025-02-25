3c53f70fce13965e22315faaa690c4af69ab7bf6
YoshiakiIshii <134984633+YoshiakiIshii@users.noreply.github.com>
2025-02-25 09:36:47 +0000

    2日目の成果

--- UnitDefineEditServer.ps1
+++ UnitDefineEditServer.ps1
@@ -1,33 +1,77 @@
-# 引数に2つのファイルパス(拡張子は.ajs)を受け取り、Webサーバーを作成して1つ目のファイルをそのままレスポンスとして返すサーバーを作成する
+# 引数にドキュメントルートディレクトリパスを受け取る
 param (
-    [Parameter(Mandatory=$true)]
-    [string]$filePath1,
-
-    [Parameter(Mandatory=$true)]
-    [string]$filePath2
+    [Parameter(Mandatory=$true)][string]$documentRootDirectoryPath
 )
 
-# 1つ目のファイルパスにファイルが存在するかチェックする(2つ目は存在しなくてよい)
-if (-Not (Test-Path $filePath1)) {
-    Write-Error "The specified file $filePath1 does not exist."
+# ディレクトリパスにディレクトリが存在するかチェックする
+if (-Not (Test-Path $documentRootDirectoryPath)) {
+    Write-Error "The specified directory $documentRootDirectoryPath does not exist."
     exit 1
 }
 
 $rootUnit = $null
 
-# シンプルなHTTPサーバーを作成する
-$port = 8080
+# HTTPサーバーを作成する
+$publishUrl = "http://localhost:8090/"
 $listener = New-Object System.Net.HttpListener
-$listener.Prefixes.Add("http://localhost:$port/")
+$listener.Prefixes.Add($publishUrl)
 $listener.Start()
-Write-Output "Listening on port $port..."
+Write-Output "Listening on port $publishUrl..."
+start $publishUrl
+
+function Send-Response {
+    param (
+        [string]$filename,
+        [string]$htmlContent,
+        [System.Net.HttpListenerResponse]$response
+    )
+
+    $targetHtmlPath = Join-Path -Path $PSScriptRoot -ChildPath $filename
+    $targetHtmlContent = Get-Content -Path $targetHtmlPath
+    $targetHtmlContent = $targetHtmlContent -replace '\$\{htmlContent\}', $htmlContent
+
+    $buffer = [System.Text.Encoding]::GetEncoding("Shift_JIS").GetBytes($targetHtmlContent)
+    $response.ContentLength64 = $buffer.Length
+    $response.OutputStream.Write($buffer, 0, $buffer.Length)
+    $response.OutputStream.Flush()
+    $response.OutputStream.Close()
+}
 
 while ($listener.IsListening) {
     $context = $listener.GetContext()
     $request = $context.Request
     $response = $context.Response
 
-    if ($request.HttpMethod -eq "GET") {
+    if ($request.HttpMethod -eq "GET" -and $request.Url.AbsolutePath -eq "/") {
+        # 入れ子構造のディレクトリ, ajsファイルをul, li, details, summaryを用いたHTML形式でレスポンスとして返す
+        function Convert-DirectoryToHtml {
+            param ([string]$directoryPath)
+            # ディレクトリの再帰的な下位にajsファイルが1つも存在しない場合は、空の文字列を返す
+            if ((Get-ChildItem -Path $directoryPath -Recurse -Filter "*.ajs").Count -eq 0) { return "" }
+
+            $html = "<li><details><summary>$(Split-Path -Leaf $directoryPath)</summary><ul>`n"
+            Get-ChildItem -Path $directoryPath | ForEach-Object {
+                if ($_.PSIsContainer) {
+                    $html += Convert-DirectoryToHtml -directoryPath $_.FullName
+                } elseif ($_.Extension -eq ".ajs") {
+                    $relativePath = $_.FullName.Substring($documentRootDirectoryPath.Length).Replace('\', '/')
+                    $html += "<li><a href='$relativePath'>$($_.Name)</a></li>`n"
+                }
+            }
+            $html += "</ul></details></li>`n"
+            return $html
+        }
+
+        Send-Response -filename "index.html" -htmlContent (Convert-DirectoryToHtml $documentRootDirectoryPath) -response $response
+        
+    } elseif ($request.HttpMethod -eq "GET" -and $request.Url.AbsolutePath -match '^/.*\.ajs$') {
+        # ドキュメントルートディレクトリパスとリクエストされたファイル名を結合してファイルパスを作成する
+        $targetFilePath = Join-Path -Path $documentRootDirectoryPath -ChildPath $request.Url.AbsolutePath.TrimStart('/')
+        if (-Not (Test-Path $targetFilePath)) {
+            $response.StatusCode = 404
+            $response.Close()
+            continue
+        }
         $response.ContentType = 'text/html; charset=Shift_JIS'
         $response.StatusCode = 200
 
@@ -46,7 +90,7 @@ while ($listener.IsListening) {
         }
         $currentUnit = $rootUnit
 
-        Get-Content -Path $filePath1 | ForEach-Object {
+        Get-Content -Path $targetFilePath | ForEach-Object {
             if ($_ -match '^\t*unit=') {
                 $parentUnit = $currentUnit
                 $currentUnit = @{
@@ -69,36 +113,44 @@ while ($listener.IsListening) {
                 [hashtable]$unit
             )
 
-            $html = ""
-            $html += "<li>`n"
-            $html += "<details open>`n"
-            $html += "<summary>$($unit.Name):$($unit.Comment)</summary>`n"
-            $html += "<ul>`n"
+            $html = "<li><details open><summary>$($unit.Name):$($unit.Comment)</summary><ul>`n"
             foreach ($nestedUnit in $unit.NestedUnits) {
                 $html += Convert-UnitToHtml -unit $nestedUnit
             }
-            $html += "</ul>`n"
-            $html += "</details>`n"
-            $html += "</li>`n"
+            $html += "</ul></details></li>`n"
             return $html
         }
 
-        $htmlContent = "<html><body>"
-        $htmlContent = "<form method='post'><input type='submit' value='Save'></form>"
-        $htmlContent += "<ul>"
-        $htmlContent += Convert-UnitToHtml -unit $rootUnit
-        $htmlContent += "</ul></body></html>"
+        Send-Response -filename "unitinfo.html" -htmlContent (Convert-UnitToHtml -unit $rootUnit.NestedUnits[0]) -response $response
 
-        $buffer = [System.Text.Encoding]::GetEncoding("Shift_JIS").GetBytes($htmlContent)
-        $response.ContentLength64 = $buffer.Length
-        $response.OutputStream.Write($buffer, 0, $buffer.Length)
-        $response.OutputStream.Flush()
-        $response.OutputStream.Close()
-    } elseif ($request.HttpMethod -eq "POST") {
+    } elseif ($request.HttpMethod -eq "GET" -and $request.Url.AbsolutePath -eq "/end") {
         $response.StatusCode = 200
         $response.Close()
+        break
 
-        # $rootUnitのNestedUnitsを再帰的に辿りながら、NameとCommentをフォーマットして$filePath2に逐次書き出す
+    } elseif ($request.HttpMethod -eq "POST") {
+        # ドキュメントルートディレクトリパスとリクエストされたファイル名を結合してファイルパスを作成する
+        $targetFilePath = Join-Path -Path $documentRootDirectoryPath -ChildPath $request.Url.AbsolutePath.TrimStart('/')
+        Write-Output $targetFilePath
+
+        # ファイルパスの拡張子がajsでない、あるいはファイルが存在しない場合は、404を返す
+        if (-Not ($targetFilePath -match '\.ajs$') -or -Not (Test-Path $targetFilePath)) {
+            $response.StatusCode = 404
+            $response.Close()
+            continue
+        }
+
+        # $filePath1の拡張子を除いたファイル名のフォルダを作成する
+        # すでに存在する場合は、フォルダ内のファイルをすべて削除する
+        $folderPath = $targetFilePath -replace '\.ajs$'
+        if (-Not (Test-Path $folderPath)) {
+            New-Item -ItemType Directory -Path $folderPath > $null
+        } else {
+            Get-ChildItem -Path $folderPath | Remove-Item -Force
+        }
+
+        $targetUnitList = $rootUnit.NestedUnits[0].NestedUnits
+
         function Write-UnitToFile {
             param (
                 [hashtable]$unit,
@@ -115,10 +167,21 @@ while ($listener.IsListening) {
             $writer.WriteLine("`t" * $depth + "}")
         }
 
-        $writer = [System.IO.StreamWriter]::new($filePath2, $false, [System.Text.Encoding]::GetEncoding("Shift_JIS"))
-        Write-UnitToFile -unit $rootUnit.NestedUnits[0] -depth 0 -writer $writer
-        $writer.Close()
+        # $targetUnitListの要素のName+".ajs"を、上で作成したフォルダに作成し、
+        # Write-UnitToFileを用いてユニット情報を書き出す
+        foreach ($unit in $targetUnitList) {
+            $filePath2 = Join-Path -Path $folderPath -ChildPath "$($unit.Name).ajs"
+            $writer = [System.IO.StreamWriter]::new($filePath2, $false, [System.Text.Encoding]::GetEncoding("Shift_JIS"))
+            Write-UnitToFile -unit $unit -depth 0 -writer $writer
+            $writer.Close()
+        }
+
+        $response.StatusCode = 200
+        $response.Close()
+
+    } else {
+        $response.StatusCode = 404
+        $response.Close()
     }
 }
 $listener.Stop()
-Write-Output "Server stopped."
